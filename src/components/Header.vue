c5c7cfaba79035de52a27ae74d3e23a0fed24b98
student-ice <tonimayloneya@gmail.com>
2024-04-12 15:41:47 +0800

    feat: add edit password dialog
    添加修改密码对话框

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,75 @@
 <script setup>
 import { useUserStore } from "@/store/index.js";
 import { ArrowDown, Fold, FullScreen, Refresh } from "@element-plus/icons-vue";
-import { useFullscreen} from "@vueuse/core";
+import { useFullscreen } from "@vueuse/core";
+import { ElMessageBox } from "element-plus";
+import { useRouter } from "vue-router";
+import { reactive, ref } from "vue";
 
-const { toggle, isFullscreen } = useFullscreen()
+const { toggle, isFullscreen } = useFullscreen();
 const userStore = useUserStore();
+const router = useRouter();
 
 const refresh = () => {
-  location.reload()
+  location.reload();
+};
+
+const handleCommand = async (res) => {
+  if (res === "editPassword"){
+    dialogEditPassword.value = true
+  }
+  if (res === "logout"){
+    const res = await ElMessageBox.confirm(
+      "是否退出登录",
+      "注意",
+      {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }
+    ).catch(err => {
+      return err
+    })
+    console.log(res);
+
+    if(res === "confirm") {
+      // 用户单击了确定按钮
+      // 删除token
+      window.sessionStorage.removeItem("token")
+      // 删除store 中的用户信息
+      userStore.setUserInfo({})
+      router.push("/login")
+    }
+  }
 }
+
+const dialogEditPassword = ref(false)
+
+const ruleFormEdit = reactive({
+  oldpassword: '',
+  password: '',
+  repassword: ''
+})
+
+const rulesEdit = reactive({
+  oldpassword: [{ required: true, message: '请输入原始密码', trigger: 'blur' }],
+  password: [{ required: true, message: '请输入新密码', trigger: 'blur' }],
+  repassword: [{ required: true, message: '请再次输入新密码', trigger: 'blur' }],
+})
+
+const ruleFormRefEdit = ref(null)
+
+const editPasswordHandle = () => {
+  dialogEditPassword.value = false
+}
+
 </script>
 
 <template>
   <div class="header">
-  <span class="logo">
-    商城后台管理系统
-  </span>
+    <span class="logo">
+      商城后台管理系统
+    </span>
     <el-icon class="icon">
       <Fold />
     </el-icon>
@@ -30,22 +84,43 @@ const refresh = () => {
           <FullScreen @click="toggle" />
         </el-icon>
       </el-tooltip>
-      <el-dropdown>
-      <span>
-        <el-avatar :size="30" :src="userStore.userInfo.avatar" />
-        {{ userStore.userInfo.username }}
-        <el-icon class="el-icon--right" style="margin-left: 10px">
-          <arrow-down />
-        </el-icon>
-      </span>
+      <el-dropdown @command="handleCommand">
+        <span>
+          <el-avatar :size="30" :src="userStore.userInfo.avatar" />
+          {{ userStore.userInfo.username }}
+          <el-icon class="el-icon--right" style="margin-left: 10px">
+            <arrow-down />
+          </el-icon>
+        </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>修改密码</el-dropdown-item>
-            <el-dropdown-item>退出登录</el-dropdown-item>
+            <el-dropdown-item command="editPassword">修改密码</el-dropdown-item>
+            <el-dropdown-item command="logout">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
     </div>
+    <el-dialog v-model="dialogEditPassword" title="修改密码" width="40%">
+      <el-form ref="ruleFormRefEdit" :model="ruleFormEdit" :rules="rulesEdit" label-width="80px">
+        <el-form-item label="原密码" prop="oldpassword">
+          <el-input v-model="ruleFormEdit.oldpassword" />
+        </el-form-item>
+        <el-form-item label="新密码" prop="password">
+          <el-input v-model="ruleFormEdit.password" />
+        </el-form-item>
+        <el-form-item label="确认密码" prop="repassword">
+          <el-input v-model="ruleFormEdit.repassword" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogEditPassword = false">取消</el-button>
+          <el-button type="primary" @click="editPasswordHandle">
+            确定
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -90,10 +165,6 @@ const refresh = () => {
     cursor: pointer;
     font-size: 18px;
     font-weight: bold;
-  }
-
-  .icon:hover {
-    background-color: #065327;
   }
 
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,14 @@
 <script setup>
-import { ElMessageBox } from "element-plus";
 import { onBeforeMount } from "vue";
-import { useUserStore } from "@/store/index.js";
-import { useRouter } from "vue-router";
 import Header from "@/components/Header.vue";
 import Menu from "@/components/Menu.vue";
 import TagMenu from "@/components/TagMenu.vue";
 
-const router = useRouter()
-const userStore = useUserStore()
-
 onBeforeMount(async () => {
 });
-
-const logout = async () => {
-  const res = await ElMessageBox.confirm(
-    "是否退出登录",
-    "注意",
-    {
-      confirmButtonText: "确定",
-      cancelButtonText: "取消",
-      type: "warning"
-    }
-  ).catch(err => {
-    return err
-  })
-  console.log(res);
-
-  if(res === "confirm") {
-    // 用户单击了确定按钮
-    // 删除token
-    window.sessionStorage.removeItem("token")
-    // 删除store 中的用户信息
-    userStore.setUserInfo({})
-    router.push("/login")
-  }
-}
 </script>
 
 <template>
-<!--  <h1>首页</h1>-->
-<!--  <el-button @click="logout">退出登录</el-button>-->
   <div style="height: 100%">
     <el-container style="height: 100%">
       <el-header>
